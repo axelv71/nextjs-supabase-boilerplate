052cbcc91a1132e2a5f38116eefb29ec95f65f3d
Axel <axel.vionyt@gmail.com>
2024-09-10 22:06:07 +0200

    Add prettier

--- src/components/auth/google-button..tsx
+++ src/components/auth/google-button..tsx
@@ -1,42 +1,37 @@
 'use client';
 
-import {Button} from "@/components/ui/button";
-import Image from "next/image";
-import Google from "@/assets/img/google.png"
-import {createClient} from "@/lib/supabase/client";
-import {useState} from "react";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Google from '@/assets/img/google.png';
+import { createClient } from '@/lib/supabase/client';
+import { useState } from 'react';
 
 export const GoogleButton = () => {
-    const [pending, setPending] = useState<boolean>(false)
+  const [pending, setPending] = useState<boolean>(false);
 
-    const connectWithGoogle = async () => {
-        const supabase = createClient();
+  const connectWithGoogle = async () => {
+    const supabase = createClient();
 
-        setPending(true);
-        await supabase.auth.signInWithOAuth({
-            provider: 'google',
-            options: {
-                redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/callback`
-            }
-        });
-        setPending(false);
-    }
+    setPending(true);
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/callback`,
+      },
+    });
+    setPending(false);
+  };
 
-    return (
-        <Button
-            variant="outline"
-            onClick={connectWithGoogle}
-            disabled={pending}
-        >
-            <Image
-                src={Google}
-                alt="Google"
-                width={18}
-                height={18}
-                className="mr-2"
-            />
-
-            Google
-        </Button>
-    )
+  return (
+    <Button variant="outline" onClick={connectWithGoogle} disabled={pending}>
+      <Image
+        src={Google}
+        alt="Google"
+        width={18}
+        height={18}
+        className="mr-2"
+      />
+      Google
+    </Button>
+  );
 };
--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -1,115 +1,100 @@
 'use client';
 
-import {cn} from "@/lib/utils";
-import {Label} from "@/components/ui/label";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {AlertCircle, LoaderCircle} from "lucide-react";
-import {useFormStatus, useFormState} from 'react-dom';
-import React from "react";
-import {signUp} from "@/actions/auth";
-import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
-import {GoogleButton} from "@/components/auth/google-button.";
+import { cn } from '@/lib/utils';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { AlertCircle, LoaderCircle } from 'lucide-react';
+import { useFormStatus, useFormState } from 'react-dom';
+import React from 'react';
+import { signUp } from '@/actions/auth';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { GoogleButton } from '@/components/auth/google-button.';
 
 interface RegisterFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export const RegisterForm = ({
-    className,
-    ...props
-}: RegisterFormProps) => {
-    const [state, action] = useFormState(signUp, {
-        errors: {}
-    })
-    const { pending } = useFormStatus();
+export const RegisterForm = ({ className, ...props }: RegisterFormProps) => {
+  const [state, action] = useFormState(signUp, {
+    errors: {},
+  });
+  const { pending } = useFormStatus();
 
-    return (
-        <div className={cn("grid gap-6", className)} {...props}>
-            <form action={action}>
-                <div className="grid gap-4">
-                    <div className="grid gap-1">
-                        <Label htmlFor="email">
-                            Email
-                        </Label>
-                        <Input
-                            id="email"
-                            name="email"
-                            placeholder="name@example.com"
-                            type="email"
-                            autoCapitalize="none"
-                            autoComplete="email"
-                            autoCorrect="off"
-                            disabled={pending}
-                        />
-                        {state.errors.email && (
-                            <Alert variant="destructive">
-                                <AlertCircle className="h-4 w-4"/>
-                                <AlertTitle>Error</AlertTitle>
-                                <AlertDescription>
-                                    {state.errors.email}
-                                </AlertDescription>
-                            </Alert>
-                        )}
-                    </div>
-                    <div className="grid gap-1">
-                        <Label htmlFor="password">
-                            Password
-                        </Label>
-                        <Input
-                            id="password"
-                            type="password"
-                            name="password"
-                            autoCapitalize="none"
-                            autoComplete="password"
-                            autoCorrect="off"
-                            disabled={pending}
-                        />
-                        {state.errors.password && (
-                            <Alert variant="destructive">
-                                <AlertCircle className="h-4 w-4"/>
-                                <AlertTitle>Error</AlertTitle>
-                                <AlertDescription>
-                                    {state.errors.password}
-                                </AlertDescription>
-                            </Alert>
-                        )}
-                    </div>
-                    {state.errors._form && (
-                        <Alert variant="destructive">
-                            <AlertCircle className="h-4 w-4"/>
-                            <AlertTitle>Error</AlertTitle>
-                            <AlertDescription>
-                                {state.errors._form}
-                            </AlertDescription>
-                        </Alert>
-                    )}
-                    <SubmitButton/>
-                </div>
-            </form>
-
-            <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                    <span className="w-full border-t"/>
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                  <span className="bg-background px-2 text-muted-foreground">
-                    Or continue with
-                  </span>
-                </div>
-            </div>
-            <GoogleButton/>
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      <form action={action}>
+        <div className="grid gap-4">
+          <div className="grid gap-1">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              name="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={pending}
+            />
+            {state.errors.email && (
+              <Alert variant="destructive">
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>{state.errors.email}</AlertDescription>
+              </Alert>
+            )}
+          </div>
+          <div className="grid gap-1">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              autoCapitalize="none"
+              autoComplete="password"
+              autoCorrect="off"
+              disabled={pending}
+            />
+            {state.errors.password && (
+              <Alert variant="destructive">
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>{state.errors.password}</AlertDescription>
+              </Alert>
+            )}
+          </div>
+          {state.errors._form && (
+            <Alert variant="destructive">
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>{state.errors._form}</AlertDescription>
+            </Alert>
+          )}
+          <SubmitButton />
         </div>
-    )
-}
+      </form>
+
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <GoogleButton />
+    </div>
+  );
+};
 
 const SubmitButton = () => {
-    const {pending} = useFormStatus();
+  const { pending } = useFormStatus();
 
-    return (
-        <Button type="submit" disabled={pending}>
-            {pending && (
-                <LoaderCircle className="mr-2 h-4 w-4 animate-spin"/>
-            )}
-            Sign Up with Email
-        </Button>
-    )
-}
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending && <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />}
+      Sign Up with Email
+    </Button>
+  );
+};
